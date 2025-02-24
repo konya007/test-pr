7ce9708df4a822a34b05eb442514c3ee30151887
trieukon21011@gmail.com <trieukon21011@gmail.com>
2025-02-24 13:36:29 +0700

    secord commit

--- Test/src/main/java/com/mycompany/test/Test.java
+++ Test/src/main/java/com/mycompany/test/Test.java
@@ -11,6 +11,8 @@ package com.mycompany.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");        
+        System.out.println("Nguyen Thanh Trieu");
+
     }
 }
